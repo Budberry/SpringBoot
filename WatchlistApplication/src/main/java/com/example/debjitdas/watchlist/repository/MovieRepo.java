8adbd6c532784eb6e1f82142ff2d2d102fbd423c
Budberry <120472905+Budberry@users.noreply.github.com>
2024-07-21 23:31:59 +0530

    MovieRepo.java

--- WatchlistApplication/src/main/java/com/example/debjitdas/watchlist/repository/MovieRepo.java
+++ WatchlistApplication/src/main/java/com/example/debjitdas/watchlist/repository/MovieRepo.java
@@ -1,9 +1,9 @@
-package com.example.kartiksapra.watchlist.repository;
+package com.example.debjitdas.watchlist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.kartiksapra.watchlist.entity.Movie;
+import com.example.debjitdas.watchlist.entity.Movie;
 
 @Repository
 public interface MovieRepo extends JpaRepository<Movie, Integer> {
